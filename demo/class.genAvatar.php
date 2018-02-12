07efc494d0ce46ee083027f5111b5e1a95741e41
Andreas Heimann <andreas@iklikla.de>
2018-02-12 14:24:27 +0100

    provided comments for explaination

--- demo/class.genAvatar.php
+++ demo/class.genAvatar.php
@@ -4,6 +4,7 @@
     private $image = null;
     private $patternSize = 8;
 
+    # Set hash for generation
     function __construct($hash = null) {
       $this->hash = $hash;
     }
@@ -12,26 +13,35 @@
       $image = @imagecreate($size, $size);
       $pattern = @imagecreate($this->patternSize, $this->patternSize);
 
+      # Convert hex color codes to rgb arrays
       $colorOneRGB = $this->hex2rgb($colorOne);
       $colorTwoRGB = $this->hex2rgb($colorTwo);
 
+      # Set ImageColors based on the colorOne and colorTwo parameters
       $background = imagecolorallocate($pattern, $colorOneRGB[0], $colorOneRGB[1], $colorOneRGB[2]);
       $pixel = imagecolorallocate($pattern, $colorTwoRGB[0], $colorTwoRGB[1], $colorTwoRGB[2]);
 
+      # Traverse through every possible pixel on the canvas
       for ($iX=0; $iX < $this->patternSize; $iX++) {
         for ($iY=0; $iY < $this->patternSize; $iY++) {
+          # change the random salt to a combination of hash, x position and y position
           mt_srand(crc32($this->hash.$iX.$iY));
+
+          # fill pixel with color if a random value between 0 and 5 is 0. This means there is a 1-4 chance for filling a pixel (~20%)
           if(mt_rand(0,5) == 0) {
             imagefilledrectangle($pattern, $iX, $iY, $iX+1, $iY+1, $pixel);
           }
         }
       }
 
+      # resize the finished canvas to the needed width/height
       imagecopyresampled($image, $pattern, 0,0,0,0,$size, $size, $this->patternSize, $this->patternSize);
 
+      # return the image data
       return imagepng($image);
     }
 
+    # function to convert hex colors to rgb arrays
     function hex2rgb($hex) {
       $hex = str_replace("#", "", $hex);
 
--- demo/generate.php
+++ demo/generate.php
@@ -1,16 +1,20 @@
 <?php
   include('class.genAvatar.php');
 
+  # Set header for image display in browser
   header("Content-type: image/png");
 
+  # generate a hash based on the current time if no hash was provided
   if(isset($_GET['hash'])) {
     $hash = $_GET['hash'];
   } else {
     $hash = time();
   }
 
+  # create a new GenAvatar instance with the provided hash
   $ga = new GenAvatar($hash);
 
+  # generate and return a GenAvatar if color1, colo2 and size are set
   if(isset($_GET['color1']) && isset($_GET['color2']) && isset($_GET['size'])) {
     echo $ga->generate($_GET['size'], $_GET['color1'], $_GET['color2']);
   }
